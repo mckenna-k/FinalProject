0153989a2f47b06908e2b639f17cc21110a8d49a
mckenna-k <kevin.ryan.mckenna@gmail.com>
2020-11-14 19:41:50 +0100

    https://github.com/mckenna-k/FinalProject.git

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Donnees pour les fonctions
+#'
+#' Un dataframe qui contient les valeurs pour les questions et reponse generique
+#' dependant de la langue choisi
+#'
+#' @format A data frame avec  17 lignes et 2 variables:
+#' \describe{
+#'      \item{France}{Tout les question en francais}
+#'      \item{USA}{Tout les questions en anglais}
+#' }
+"paysdf"
