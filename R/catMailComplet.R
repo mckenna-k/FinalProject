a8598f61a7419c2c5d370b0d19eb791f1a134869
mckenna-k <kevin.ryan.mckenna@gmail.com>
2020-11-30 12:07:13 +0100

    BuildAgain

---
+++ R/catMailComplet.R
@@ -0,0 +1,23 @@
+#' catMailComplet
+#'
+#' @param mail
+#' mail doit etre un string de caracters avec une arobase
+#' @param Langue
+#' Langue doit etre un string de caracters
+#'
+#' @export
+#' @description
+#' catMailComplet prend les valeurs et test pour voir si il y a une arobase.
+#' Si il n'y a pas de arobase, il renvoit une message d'erreur
+#'
+#' @examples
+#' catNomComplet("McKenna.kevin.gmail.com","France")
+#' [1] Erreur: pas une format propre pour un mail
+#'
+catMailComplet <- function(mail, langue){
+  if (is.null(mail) || !stringr::str_detect(mail, "@")) {
+    as.character(paysdf[[langue]][17])
+  } else {
+    mail
+  }
+}
---
+++ R/catNumeroComplet.R
@@ -0,0 +1,49 @@
+#' catNumeroComplet
+#'
+#' @param countryCode
+#' nom doit etre un chaine de caracters de chiffres
+#' @param tonNumero
+#' tonNumero doit etre un chaine de caracters de 10 chiffres
+#' @param Langue
+#' Langue doit etre un string de caracters
+#'
+#' @export
+#' @description
+#' catNomComplet prend les valeurs du countryCode et tonNumero et les concatent ensembles.
+#' Il le met dans la format du pays choisi (soit France, soit USA)
+#'
+#' @examples
+#' catNumeroComplet("33","0123456789","France")
+#' [1] "+33 1-23-45-67-89"
+#' catNumeroComplet("33","0123456789","USA")
+#' [1] "+33 012-345-6789"
+#'
+catNumeroComplet <- function(countryCode, tonNumero, Langue){
+  paste(if (is.null(countryCode)) {
+    languageInput()[16]
+  } else if (stringr::str_detect(countryCode, "^\\+") == T) {
+    if (comparaisonChiffres(substring(countryCode, 2))) {
+      countryCode
+    } else {
+      languageInput()[16]
+    }
+  } else if (comparaisonChiffres(countryCode)) {
+    paste0("+", countryCode)
+  } else {
+    languageInput()[16]
+  },
+  if (is.null(tonNumero) ||
+      !comparaisonChiffres(tonNumero)) {
+    languageInput()[16]
+  } else {
+    if (stringr::str_count(tonNumero, "[0123456789]") == 10) {
+      if (Langue == "France") {
+        numeroFrancais(tonNumero)
+      } else {
+        numeroAmericain(tonNumero)
+      }
+    } else {
+      languageInput()[15]
+    }
+  })
+}
