39d668e7d0de228c6560aa42fb020056f6c51920
mckenna-k <kevin.ryan.mckenna@gmail.com>
2020-12-01 13:59:12 +0100

    data added

---
+++ R/skills.R
@@ -0,0 +1,3 @@
+skills<-data.frame("France"=c("Langues","Programmation","Permis","Logiciel","Autre"),
+                   "USA"=c("Language","Computer science","License","Software","Other"),
+                   row.names=c("Langue","Prog","Permis","soft","other"))
