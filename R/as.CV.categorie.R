35175cff214d8f5f413ae2e8a31e1cec969a85d1
mckenna-k <kevin.ryan.mckenna@gmail.com>
2020-12-10 22:26:00 +0100

    basic version

--- R/as.CV.categorie.R
+++ R/as.CV.categorie.R
@@ -15,8 +15,8 @@ as.CV.categorie <- function(dataTxt, pays) {
   }
   #if(is.null(dataTxt)){return(null)}
   data <- dataTxt %>%
-    tidyr::separate(1, c("type", names(dataTxt)[1]), sep = ":") %>%
-    tidyr::separate(2, c(names(dataTxt)[1], message["level", pays]), sep = paste0(", ", message["level", pays]))
+    separate(1, c("type", names(dataTxt)[1]), sep = ":") %>%
+    separate(2, c(names(dataTxt)[1], message["level", input$Langue]), sep = paste0(", ", message["level", input$Langue]))
   data[, 3] <- as.numeric(data[, 3])
   class(data) <-
     append(class(data), ifelse(pays == "France", "CV.FR", "CV.US"))
